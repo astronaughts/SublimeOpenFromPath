100603439e672ffea28434ee6013f59cafcf635c
masahiro ueno <astronaughts@masahiro-no-MacBook-Pro.local>
2012-10-29 17:05:02 +0900

    add choose from history. add key-maps.

--- OpenFromPath.py
+++ OpenFromPath.py
@@ -2,17 +2,41 @@ import sublime
 import sublime_plugin
 import os.path
 
-class OpenCommand(sublime_plugin.WindowCommand):
+history = []
 
-    def run(self):
-        self.window.show_input_panel('Path:', '', self.open, None, None)
+class OpenFromPathCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        panel = self.view.window().show_input_panel(
+            'Open from ...',
+            history[0] if history else '',
+            self.open,
+            None,
+            None,
+        )
 
     def open(self, path):
-
         try:
-            if not os.path.isfile(path):
-                raise IOError1
+            self.view.window().open_file(path)
+            history.append(path)
+        except Exception as e:
+            sublime.error_message('OpenFromPath [Error]: Could not open this file.')
 
-            self.window.open_file(path)
-        except:
-            sublime.error_message('OpenFromPath Error: Could not open this file.')
+class OpenFromHistoryCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        if not history:
+            sublime.error_message('OpenFromPath [Error]: There is no history.')
+            return
+
+        summary = [item.strip()[:100] for item in history]
+        self.view.window().show_quick_panel(summary, self.open)
+
+    def open(self, index):
+        try:
+            if not history:
+                return
+
+            self.view.window().open_file(history[index])
+        except Exception as e:
+            sublime.error_message('OpenFromPath [Error]: Could not open this file.')
